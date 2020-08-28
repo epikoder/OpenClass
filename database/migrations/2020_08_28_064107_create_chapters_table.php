b0c7d47d6535d7705b9e362fe23f6f504ca35309
eribell <efedua.bell@gmail.com>
2020-08-28 07:50:50 +0100

    init

---
+++ database/migrations/2020_08_28_064107_create_chapters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChaptersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('courses_id');
+            $table->string('name');
+            $table->unsignedInteger('chapter_num');
+            $table->timestamps();
+
+            $table->foreign('courses_id')
+            ->references('id')
+            ->on('courses')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
---
+++ database/migrations/2020_08_28_064518_create_pages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chapters_id');
+            $table->string('name');
+            $table->unsignedInteger('page_num');
+            $table->timestamps();
+
+            $table->foreign('chapters_id')
+            ->references('id')
+            ->on('chapters')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
